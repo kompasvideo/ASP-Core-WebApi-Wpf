484d0fd23656788ccb92b8d82d45cd3cde08a21d
Андрей Иванович Барышников <kompasvideo@yandex.ru>
2021-11-14 15:26:39 +0300

    Добавьте файлы проекта.

---
+++ Controllers/PhoneBookController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeWork_22.Models;
+
+namespace HomeWork_22.Controllers
+{
+    public class PhoneBookController : Controller
+    {
+        private IPhoneBookRepository repository;
+
+        public PhoneBookController(IPhoneBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(repository.PhoneBooks);
+        }
+        [HttpPost]
+        public RedirectToActionResult DeleteRecord(int id)
+        {
+            repository.DeleteRecord(id);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public RedirectToActionResult AddRecord(string LastName, string FirstName, string ThreeName, string NumberPhone,
+            string Address, string Description)
+        {
+            PhoneBook phoneBook = new PhoneBook();
+            phoneBook.FirstName = FirstName;
+            phoneBook.LastName = LastName;
+            phoneBook.ThreeName = ThreeName;
+            phoneBook.NumberPhone = NumberPhone;
+            phoneBook.Address = Address;
+            phoneBook.Description = Description;
+            repository.SaveRecord(phoneBook);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public RedirectToActionResult EditRecord(int id, string LastName, string FirstName, string ThreeName, string NumberPhone,
+            string Address, string Description)
+        {
+            PhoneBook phoneBook = new PhoneBook();
+            phoneBook.PhoneBookID = id;
+            phoneBook.FirstName = FirstName;
+            phoneBook.LastName = LastName;
+            phoneBook.ThreeName = ThreeName;
+            phoneBook.NumberPhone = NumberPhone;
+            phoneBook.Address = Address;
+            phoneBook.Description = Description;
+            repository.SaveRecord(phoneBook);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/RecordController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeWork_22.Models;
+
+namespace HomeWork_22.Controllers
+{
+    public class RecordController : Controller
+    {
+        private IPhoneBookRepository repository;
+        public RecordController(IPhoneBookRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(PhoneBook phoneBook)
+        {
+           return View(phoneBook);
+        }
+
+        public RedirectToActionResult ViewRecord(int id)
+        {
+            PhoneBook phoneBook = repository.PhoneBooks
+                .FirstOrDefault(p => p.PhoneBookID == id);
+            return RedirectToAction("Index", phoneBook);
+        }
+
+        public ViewResult ViewAddRecord()
+        {
+            return View();
+        }
+        public ViewResult ViewEditRecord(int id)
+        {
+            PhoneBook phoneBook = repository.PhoneBooks
+                .FirstOrDefault(p => p.PhoneBookID == id);
+            return View(phoneBook);
+        }
+    }
+}
