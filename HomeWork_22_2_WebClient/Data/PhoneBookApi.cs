2139f7b368e2c6cd9c1ab71358a00b6690128663
Андрей Иванович Барышников <kompasvideo@yandex.ru>
2021-12-09 00:17:51 +0300

    v1.7

---
+++ HomeWork_22_2_WebClient/Data/PhoneBookApi.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace HomeWork_22_2_WebClient.Data
+{
+    public class PhoneBookApi : IPhoneBook
+    {
+        private HttpClient httpClient { get; set; }
+
+        public IEnumerable<PhoneBook> phoneBooks { get; set; }
+        PhoneBook phoneBooksRecord { get; set; }
+
+        public PhoneBookApi()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public IEnumerable<PhoneBook> GetPhoneBook()
+        {
+            string url = @"https://localhost:5001/PhoneBook";
+
+            string json = httpClient.GetStringAsync(url).Result;
+
+            phoneBooks = JsonConvert.DeserializeObject<IEnumerable<PhoneBook>>(json);
+            return phoneBooks;
+        }
+
+        public PhoneBook GetPhoneBookId(int id)
+        {
+            string url = @"https://localhost:5001/PhoneBook/" + id.ToString();
+
+            string json = httpClient.GetStringAsync(url).Result;
+
+            phoneBooksRecord = JsonConvert.DeserializeObject<PhoneBook>(json);
+            return phoneBooksRecord;
+        }
+
+        public void AddAndEditRecord(PhoneBook phoneBook)
+        {
+            string url = @"https://localhost:5001/PhoneBook";
+
+            var r = httpClient.PostAsync(
+                requestUri: url,
+                content: new StringContent(JsonConvert.SerializeObject(phoneBook), Encoding.UTF8,
+                mediaType: "application/json")
+                ).Result;
+        }
+
+        public void DeleteRecord(int id)
+        {
+            string url = @"https://localhost:5001/PhoneBook/" + id.ToString();
+
+            var r = httpClient.PostAsync(
+                requestUri: url,
+                content: new StringContent(id.ToString(), Encoding.UTF8,
+                mediaType: "application/json")
+                ).Result;
+        }
+    }
+}
---
+++ HomeWork_22_2_WebClient/Interfaces/IPhoneBook.cs
@@ -0,0 +1,13 @@
+﻿using HomeWork_22_2_WebClient;
+
+namespace HomeWork_22_2_WebClient.Interfaces
+{
+    public interface IPhoneBook
+    {
+        IEnumerable<PhoneBook> phoneBooks { get; set; }
+        IEnumerable<PhoneBook> GetPhoneBook();
+        PhoneBook GetPhoneBookId( int id);
+        void AddAndEditRecord(PhoneBook phoneBook);
+        void DeleteRecord(int id);
+    }
+}
